8f2d9a9de1d9771525a8b443410442b5cd86e5af
your name <you@example.com>
2022-02-28 12:01:29 +0100

    Add project files.

---
+++ Projekt_aflevering/Codes/Models/Bil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_aflevering.Codes.Models
+{
+    internal class Bil
+    {
+
+        public string Mærke { get; set; }
+        public string Model { get; set; }
+        public int HesteKræfter { get; set; }
+        
+    }
+}
---
+++ Projekt_aflevering/Codes/MotorKøretøj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_aflevering.Codes
+{
+    internal class MotorKøretøj
+    {
+
+        public List<Bil> Biler { get; set; }
+
+
+    }
+}
